40da648efcfa4ccbdede9bc1168ded7a37cf6341
Levi Yourchuck <levi.yourchuck@gmail.com>
2022-11-29 18:37:41 -0600

    update install and uninstall scripts

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript kwin-rectangle || true
+qdbus-qt5 org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript kwin-rectangle || true
 
 plasmapkg2 -t kwinscript -r kwin-rectangle || true
 plasmapkg2 -t kwinscript -i .
 
-qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript \
+qdbus-qt5 org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript \
     ~/.local/share/kwin/scripts/kwin-rectangle kwin-rectangle
-qdbus org.kde.KWin /KWin org.kde.KWin.reconfigure
-qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.start
+qdbus-qt5 org.kde.KWin /KWin org.kde.KWin.reconfigure
+qdbus-qt5 org.kde.KWin /Scripting org.kde.kwin.Scripting.start
 
 if [ ! -f "/usr/share/kservices5/kwin-rectangle.desktop" ]; then
     echo "I will now create a symlink on a system folder."
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 plasmapkg2 -t kwinscript -r kwin-rectangle
-qdbus org.kde.kglobalaccel /component/kwin cleanUp
+qdbus-qt5 org.kde.kglobalaccel /component/kwin cleanUp
 
 if [ ! -f "/usr/share/kservices5/kwin-rectangle.desktop" ]; then
     echo "I will now remove a symlink on a system folder, since it is no longer needed."
